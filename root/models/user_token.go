a2f903b5c41dd5224f075a93c3640ad86bfd5c28
Garg-Shashwat <gargshashwat007@gmail.com>
2024-11-24 06:41:43 +0530

    chore: add models
    add go mod, gorm and schema models

---
+++ root/models/user_token.go
@@ -0,0 +1,16 @@
+package models
+
+import (
+	"time"
+
+	"gorm.io/gorm"
+)
+
+type UserToken struct {
+	gorm.Model
+	UserID    uint
+	User      User
+	Token     string
+	ExpiresAt time.Time
+	IsRevoked bool
+}
